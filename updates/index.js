a5e6f29bcc703778e638a1c7018fdf78318640eb
Jack He <jackhe2345@gmail.com>
2020-02-19 02:04:03 -0800

    added react

---
+++ updates/index.js
@@ -0,0 +1,25 @@
+/*
+    SIMPLY CHANGE THE TEXT INSIDE THE DIV TAG IN ORDER TO UPDATE THE SITE WITH NEW INFORMATION
+*/
+
+
+// CHANGE THE MAIN TITLE 
+function UpdateTitle() {
+    return (
+        <div>
+            Incline Your Education
+        </div>
+    )
+}
+
+// CHANGE THE SUB TITLE
+function UpdateSubTitle() {
+    return (
+        <div>
+            From Kindergarten to University
+        </div>
+    )
+}
+
+ReactDOM.render(<UpdateTitle />, document.getElementById("title"))
+ReactDOM.render(<UpdateSubTitle />, document.getElementById("subtitle"))
