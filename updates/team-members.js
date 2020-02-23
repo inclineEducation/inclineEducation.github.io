46870a5105ccd5b01609aab20b7f2e436a57839e
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-02-23 14:45:28 -0800

    formats

--- updates/team-members.js
+++ updates/team-members.js
@@ -14,6 +14,7 @@ LIST OF ALL MEMBERS HERE:
 const NAME_1 = "Christopher Ng"
 const DESCRIPTION_1 = "Christopher is a second year student pursuing a Bachelor of Science in Pharmacology and Minor in Commerce. He has previously interned in Deloitte Consulting and is currently working on publishing 2 papers in medical research. Outside of the classroom, Christopher can be found playing squash, running or enjoying the outdoors."
 const IMAGE_URL_1 = "images/chris.png"
+const PERSONAL_PAGE_1 = "#"
 // ---
 
 
@@ -22,6 +23,7 @@ const IMAGE_URL_1 = "images/chris.png"
 const NAME_2 = "Jack He"
 const DESCRIPTION_2 = "Jack is a first year student at UBC pursuing a Bachelor of Science in Computer Science and a Minor in Commerce. He will be interning at Microsoft as a software engineer and product manager this summer. Jack is currently working on his YouTube channel and will be posting about his journey to launching a Silicon Valley startup in the future. Jack enjoys playing chess and watching anime."
 const IMAGE_URL_2 = "images/jack.jpg"
+const PERSONAL_PAGE_2 = "#"
 // ---
 
 
@@ -29,6 +31,7 @@ const IMAGE_URL_2 = "images/jack.jpg"
 const NAME_3 = "Talisha Griesbach"
 const DESCRIPTION_3 = "Talisha is in her second year of Integrated Engineering in UBC. She designs Printed Circuit Boards with UBC Supermileage, serves as Director of Communications in Alpha Gamma Delta, works as a Collegia Advisor, and started her own social enterprise, Patch. Talisha is looking forward to balancing her summer co-op with Kardium while exercising and playing cello."
 const IMAGE_URL_3 = "images/Talisha_edited.jpg"
+const PERSONAL_PAGE_3 = "#"
 // ---
 
 
@@ -36,6 +39,7 @@ const IMAGE_URL_3 = "images/Talisha_edited.jpg"
 const NAME_4 = "Michelle Lin"
 const DESCRIPTION_4 = "Michelle is a 2nd year Biomedical Engineering Student at UBC. She previously served as the First Year Council President at UBC Engineering. She has been an ambassador of Engineering Stories underneath the Dean’s Office and has also started her own fundraising initiative that funds elementary schools. Outside of school, her passions are playing Tennis and Film making."
 const IMAGE_URL_4 = "images/Michelle.jpg"
+const PERSONAL_PAGE_4 = "#"
 // ---
 
 
@@ -43,6 +47,7 @@ const IMAGE_URL_4 = "images/Michelle.jpg"
 const NAME_5 = "Matheson Parmar"
 const DESCRIPTION_5 = "Matheson is a second year student pursuing a Bachelor of Commerce in Accounting and Finance. He is currently working on a startup and has written articles on the consumer-goods market. Outside of work and the classroom, Matheson can be found running, solving cases, or self-teaching piano."
 const IMAGE_URL_5 = "images/matheson.jpg"
+const PERSONAL_PAGE_5 = "#"
 // ---
 
 
@@ -50,6 +55,7 @@ const IMAGE_URL_5 = "images/matheson.jpg"
 const NAME_6 = "Anushka Gupta"
 const DESCRIPTION_6 = "Anushka is a second-year student in the Bachelor of International Economics Program. She is the founder of a non-profit called Aan which works with the government of India to conduct campaigns about child sexual abuse. She worked as a Research and Data Analysis Assistant at the Vancouver School of Economics Career Center and hopes to work in the field of developmental economics in the public sector."
 const IMAGE_URL_6 = "images/Anushka.jpg"
+const PERSONAL_PAGE_6 = "#"
 // ---
 
 
@@ -58,6 +64,7 @@ const IMAGE_URL_6 = "images/Anushka.jpg"
 const NAME_7 = "DANILO ANGULO-MOLINA"
 const DESCRIPTION_7 = "Danilo is a second-year student pursuing a Bachelor of Arts in International Relations and Political Sciences. He has previously been a youth ambassador of Colombia to multiple countries. He is the director and host of Let's Talk About, an initiative that gives a voice to students. Outside of school, Danilo can be found watching sunsets, practicing languages and swimming."
 const IMAGE_URL_7 = "images/danilo.jpg"
+const PERSONAL_PAGE_7 = "#"
 // ---
 
 
@@ -66,12 +73,14 @@ const IMAGE_URL_7 = "images/danilo.jpg"
 const NAME_8 = "ANDY CHUNG"
 const DESCRIPTION_8 = "Andy is a second year student pursuing a Bachelor of Applied Sciences in Mechanical Engineering with a Thermofluids specialization. He has previously partnered with Telus during his internship with Legacy Fire Protection and is currently the Engine Head of Design at Formula UBC Racing. Outside of academics, Andy enjoys playing the guitar, practicing karate, and participating in motorsports."
 const IMAGE_URL_8 = "images/andy.jpg"
+const PERSONAL_PAGE_8 = "#"
 // ---
 
 //MEMBER 9
 const NAME_9 = "ROY DU"
 const DESCRIPTION_9 = "Roy is a second year student pursuing a Bachelor of Applied Sciences in Computer Engineering at UBC. He is a hardware and software developer on the UBC Supermileage team, and is currently serving as the 2nd year Computer Engineering Representative for the ECESS. Outside of school, Roy is an avid snowboarder, and is frequently found on Reddit for extended durations of time."
 const IMAGE_URL_9 = "images/Roy.png"
+const PERSONAL_PAGE_9 = "http://bit.ly/notsusatall"
 // ---
 
 function Member(props) {
@@ -93,15 +102,15 @@ class Team extends React.Component {
     render() {
         return (
             <div className = "row">
-                <Member name = {NAME_1} description = {DESCRIPTION_1} img = {IMAGE_URL_1} website = "#" delay = {100} />
-                <Member name = {NAME_2} description = {DESCRIPTION_2} img = {IMAGE_URL_2} website = "http://jackhe.codes" delay = {200} />
-                <Member name = {NAME_3} description = {DESCRIPTION_3} img = {IMAGE_URL_3} website = "#" delay = {300} />
-                <Member name = {NAME_4} description = {DESCRIPTION_4} img = {IMAGE_URL_4} website = "#" delay = {100} />
-                <Member name = {NAME_5} description = {DESCRIPTION_5} img = {IMAGE_URL_5} website = "#" delay = {200} />
-                <Member name = {NAME_6} description = {DESCRIPTION_6} img = {IMAGE_URL_6} website = "#" delay = {300} />
-                <Member name = {NAME_7} description = {DESCRIPTION_7} img = {IMAGE_URL_7} website = "#" delay = {100} />
-                <Member name = {NAME_8} description = {DESCRIPTION_8} img = {IMAGE_URL_8} website = "#" delay = {200} />
-				<Member name = {NAME_9} description = {DESCRIPTION_9} img = {IMAGE_URL_9} website = "http://bit.ly/notsusatall" delay = {300} />
+                <Member name = {NAME_1} description = {DESCRIPTION_1} img = {IMAGE_URL_1} website = {PERSONAL_PAGE_1} delay = {100} />
+                <Member name = {NAME_2} description = {DESCRIPTION_2} img = {IMAGE_URL_2} website = {PERSONAL_PAGE_2} delay = {200} />
+                <Member name = {NAME_3} description = {DESCRIPTION_3} img = {IMAGE_URL_3} website = {PERSONAL_PAGE_3} delay = {300} />
+                <Member name = {NAME_4} description = {DESCRIPTION_4} img = {IMAGE_URL_4} website = {PERSONAL_PAGE_4} delay = {100} />
+                <Member name = {NAME_5} description = {DESCRIPTION_5} img = {IMAGE_URL_5} website = {PERSONAL_PAGE_5} delay = {200} />
+                <Member name = {NAME_6} description = {DESCRIPTION_6} img = {IMAGE_URL_6} website = {PERSONAL_PAGE_6} delay = {300} />
+                <Member name = {NAME_7} description = {DESCRIPTION_7} img = {IMAGE_URL_7} website = {PERSONAL_PAGE_7} delay = {100} />
+                <Member name = {NAME_8} description = {DESCRIPTION_8} img = {IMAGE_URL_8} website = {PERSONAL_PAGE_8} delay = {200} />
+				<Member name = {NAME_9} description = {DESCRIPTION_9} img = {IMAGE_URL_9} website = {PERSONAL_PAGE_9} delay = {300} />
             </div>
         )
     }
