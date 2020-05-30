7a3ec075e3710f2fda4724c2afa88cba121efe09
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-05-30 14:59:04 -0700

    Change JQuery to properly highlight nav-links without html in name

--- js/element-updates.js
+++ js/element-updates.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('.nav-link').each(function() {
-        if ($(this).attr('href') == location.href.split("/").slice(-1)){ $(this).addClass('active'); }
+        if ($(this).attr('href').split('.html')[0] == location.href.split("/").slice(-1)[0].split('.html')[0]){
+             $(this).addClass('active');
+        }
     });
 });
