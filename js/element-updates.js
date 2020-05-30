3da73ac07fe3e798c9a888c02d7e4f3c0527db7a
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-05-30 15:59:45 -0700

    removed debug lines

--- js/element-updates.js
+++ js/element-updates.js
@@ -4,8 +4,6 @@
 */
 function updateNavbar(){
     $('.nav-link').each(function() {
-        console.log('navbar item: ' + $(this).attr('href').split('.html')[0]);
-        console.log('current page: ' + location.href.split("/").slice(-1)[0].split('.html')[0]);
         if (($(this).attr('href').split('.html')[0] == location.href.split("/").slice(-1)[0].split('.html')[0]) ||
         ($(this).attr('href').split('.html')[0] == 'index' && location.href.split("/").slice(-1)[0].split('.html')[0] == '')){
              $(this).addClass('active');
