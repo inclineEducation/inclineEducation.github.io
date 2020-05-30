3ff6b5af1690de7e29215ac331b4f478916dc50d
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-05-30 15:27:03 -0700

    Add observer to element-updates.js to ensure altercations to nav-bar occurs AFTER nav-bar is created

--- js/element-updates.js
+++ js/element-updates.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
     console.log('document ready');
-    $('.nav-link').each(function() {
-        console.log('navbar item: ' + $(this).attr('href').split('.html')[0]);
-        console.log('current page: ' + location.href.split("/").slice(-1)[0].split('.html')[0]);
-        if (($(this).attr('href').split('.html')[0] == location.href.split("/").slice(-1)[0].split('.html')[0]) ||
-        ($(this).attr('href').split('.html')[0] == 'index' && location.href.split("/").slice(-1)[0].split('.html')[0] == '')){
-             $(this).addClass('active');
+    var observer = new MutationObserver(function (mutations, me) {
+        var canvas = document.getElementsByClassName('nav-link');
+        if (canvas) {
+            $('.nav-link').each(function() {
+                console.log('navbar item: ' + $(this).attr('href').split('.html')[0]);
+                console.log('current page: ' + location.href.split("/").slice(-1)[0].split('.html')[0]);
+                if (($(this).attr('href').split('.html')[0] == location.href.split("/").slice(-1)[0].split('.html')[0]) ||
+                ($(this).attr('href').split('.html')[0] == 'index' && location.href.split("/").slice(-1)[0].split('.html')[0] == '')){
+                     $(this).addClass('active');
+                }
+            });
+
+            me.disconnect;
+            return;
         }
     });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    })
+    
 });
