b351f386b092590860a3e6eb49e00d4b16f6fdd2
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-05-30 15:52:56 -0700

    made updateNavbar into a function, and added functionality to load navbar even if no updates needed

--- js/element-updates.js
+++ js/element-updates.js
@@ -1,25 +1,38 @@
-$(document).ready(function(){
-    console.log('document ready');
-    var observer = new MutationObserver(function (mutations, me) {
-        var canvas = document.getElementsByClassName('nav-link');
-        if (canvas) {
-            $('.nav-link').each(function() {
-                console.log('navbar item: ' + $(this).attr('href').split('.html')[0]);
-                console.log('current page: ' + location.href.split("/").slice(-1)[0].split('.html')[0]);
-                if (($(this).attr('href').split('.html')[0] == location.href.split("/").slice(-1)[0].split('.html')[0]) ||
-                ($(this).attr('href').split('.html')[0] == 'index' && location.href.split("/").slice(-1)[0].split('.html')[0] == '')){
-                     $(this).addClass('active');
-                }
-            });
-
-            me.disconnect();
-            return;
+/*
+* function updateNavbar()
+* activates current page in navbar
+*/
+function updateNavbar(){
+    $('.nav-link').each(function() {
+        console.log('navbar item: ' + $(this).attr('href').split('.html')[0]);
+        console.log('current page: ' + location.href.split("/").slice(-1)[0].split('.html')[0]);
+        if (($(this).attr('href').split('.html')[0] == location.href.split("/").slice(-1)[0].split('.html')[0]) ||
+        ($(this).attr('href').split('.html')[0] == 'index' && location.href.split("/").slice(-1)[0].split('.html')[0] == '')){
+             $(this).addClass('active');
         }
     });
+}
+
+//wait for navbar to load, then update it.
+$(document).ready(function(){
+    console.log('document ready');
+    var canvas = document.getElementsByClassName('nav-link');
+    if (canvas) {
+        updateNavbar();
+        return;
+    } else {
+        var observer = new MutationObserver(function (mutations, me) {
+            var canvas = document.getElementsByClassName('nav-link');
+            if (canvas) {
+                updateNavbar();
+                me.disconnect();
+                return;
+            }
+        });
+    }
 
     observer.observe(document, {
         childList: true,
         subtree: true
-    })
-    
+    });
 });
