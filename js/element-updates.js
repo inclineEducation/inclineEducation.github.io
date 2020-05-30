5004553bd9d5a6261cbdbb7486549c551bcc0996
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-05-30 15:09:15 -0700

    more debug

--- js/element-updates.js
+++ js/element-updates.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    console.log('document ready');
     $('.nav-link').each(function() {
         console.log('navbar item: ' + $(this).attr('href').split('.html')[0]);
         console.log('current page: ' + location.href.split("/").slice(-1)[0].split('.html')[0]);
