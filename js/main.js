21e8a34c59cbf9f90563d89efcb2f32413e5a958
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-06-20 22:04:45 -0700

    formatted javascript and changed about us functionality

--- js/main.js
+++ js/main.js
@@ -1,159 +1,171 @@
- AOS.init({
+AOS.init({
   duration: 800,
-  easing: 'slide-up',
-  once: true
- });
-(function($) {
+  easing: "slide-up",
+  once: true,
+});
+(function ($) {
+  "use strict";
 
-	'use strict';
-
-	// bootstrap dropdown hover
+  // bootstrap dropdown hover
 
   // loader
-  var loader = function() {
-    setTimeout(function() { 
-      if($('#loader').length > 0) {
-        $('#loader').removeClass('show');
+  var loader = function () {
+    setTimeout(function () {
+      if ($("#loader").length > 0) {
+        $("#loader").removeClass("show");
       }
     }, 1);
   };
   loader();
 
-	
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
-	});
+  $("nav .dropdown").hover(
+    function () {
+      var $this = $(this);
+      $this.addClass("show");
+      $this.find("> a").attr("aria-expanded", true);
+      $this.find(".dropdown-menu").addClass("show");
+    },
+    function () {
+      var $this = $(this);
+      $this.removeClass("show");
+      $this.find("> a").attr("aria-expanded", false);
+      $this.find(".dropdown-menu").removeClass("show");
+    }
+  );
 
-  $('.navbar .dropdown > a').click(function(){
+  $(".navbar .dropdown > a").click(function () {
     location.href = this.href;
   });
 
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
+  $("#dropdown04").on("show.bs.dropdown", function () {
+    console.log("show");
   });
-  
-  
-	// home slider
-	$('.home-slider').owlCarousel({
-    loop:true,
+
+  // home slider
+  $(".home-slider").owlCarousel({
+    loop: true,
     autoplay: true,
-    margin:0,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav:true,
+    margin: 0,
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
+    nav: true,
     autoplayHoverPause: true,
     items: 1,
     dragTouch: false,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: [
+      "<span class='ion-chevron-left'></span>",
+      "<span class='ion-chevron-right'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-        items:1,
-        nav:false
+      600: {
+        items: 1,
+        nav: false,
       },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-	});
+      1000: {
+        items: 1,
+        nav: true,
+      },
+    },
+  });
 
-  $('.nonloop-block-11').owlCarousel({
-      center: false,
-      items: 1,
-      loop: true,
-      autoplay: true,
-      stagePadding: 20,
-      margin:50,
-      nav: false,
-      dots: true,
-      smartSpeed: 1000,
-      navText: ['<span class="ion-chevron-left">', '<span class="ion-chevron-right">'],
-      responsive:{
-        600:{
-          stagePadding: 20,
-          items:1
-        },
-        800:{
-          stagePadding: 20,
-          items:2
-        },
-        1000:{
-          items:2
-        }
-      }
-    });
+  $(".nonloop-block-11").owlCarousel({
+    center: false,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    stagePadding: 20,
+    margin: 50,
+    nav: false,
+    dots: true,
+    smartSpeed: 1000,
+    navText: [
+      '<span class="ion-chevron-left">',
+      '<span class="ion-chevron-right">',
+    ],
+    responsive: {
+      600: {
+        stagePadding: 20,
+        items: 1,
+      },
+      800: {
+        stagePadding: 20,
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
 
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var majorCarousel = $(".js-carousel-1");
+  majorCarousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
     nav: true,
     autoplayHoverPause: true,
     items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: [
+      "<span class='ion-chevron-left'></span>",
+      "<span class='ion-chevron-right'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-        items:2,
-        nav:false
+      600: {
+        items: 2,
+        nav: false,
       },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
 
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var major2Carousel = $(".js-carousel-2");
+  major2Carousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
     nav: true,
     autoplayHoverPause: true,
     items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: [
+      "<span class='ion-chevron-left'></span>",
+      "<span class='ion-chevron-right'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-        items:3,
-        nav:false
+      600: {
+        items: 3,
+        nav: false,
       },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
 
   var siteStellar = function () {
     $(window).stellar({
@@ -162,60 +174,57 @@
       parallaxElements: true,
       horizontalScrolling: false,
       hideDistantElements: false,
-      scrollProperty: 'scroll'
+      scrollProperty: "scroll",
     });
   };
   siteStellar();
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.element-animate').waypoint( function( direction ) {
+  var contentWayPoint = function () {
+    var i = 0;
+    $(".element-animate").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("element-animated")
+        ) {
+          i++;
 
-			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
-				
-				i++;
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .element-animate.item-animate").each(function (k) {
+              var el = $(this);
+              setTimeout(function () {
+                var effect = el.data("animate-effect");
+                if (effect === "fadeIn") {
+                  el.addClass("fadeIn element-animated");
+                } else if (effect === "fadeInLeft") {
+                  el.addClass("fadeInLeft element-animated");
+                } else if (effect === "fadeInRight") {
+                  el.addClass("fadeInRight element-animated");
+                } else {
+                  el.addClass("fadeInUp element-animated");
+                }
+                el.removeClass("item-animate");
+              }, k * 100);
+            });
+          }, 100);
+        }
+      },
+      { offset: "95%" }
+    );
+  };
+  contentWayPoint();
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .element-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn element-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft element-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight element-animated');
-							} else {
-								el.addClass('fadeInUp element-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 100);
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
-
-  var slideImage = function() {
+  var slideImage = function () {
     $(".slide-image").each(function () {
       var bg = $(this).data("bg");
       var pos = $(this).data("kenburn-start");
 
       $(this).css({
         "background-image": "url(" + bg + ")",
-        "transform-origin": pos
+        "transform-origin": pos,
       });
     });
   };
   slideImage();
-  
-  
-
 })(jQuery);
