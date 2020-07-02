73776aee5dc0d53123f561d9f02bcdc275c1f050
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-07-02 00:54:24 -0700

    php

--- contactsub.php
+++ contactsub.php
@@ -35,10 +35,94 @@
     $mail = $smtp->send($to, $headers, $body);
 
     if (PEAR::isError($mail)) {
-        echo("<p>" . $mail->getMessage() . "</p>");
+        $mailmessage = $mail->getMessage();
        } else {
-        echo("<p>Message successfully sent!</p>");
+        $mailmessage = "Thank you! We have received your message!";
        }
     
-    header("Location: /contact");
+    header("refresh:5; /contact");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>404-PAGE NOT FOUND</title>
+  <meta name="keywords" content="incline,education,mentorship,panel,discussions,secondary,education,school,
+  post,high,students,teachers,parents,community,advancing,charity,blog,university,panel">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="shortcut icon" href="/ftco-32x32.png">
+
+  <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display:400,400i|Roboto+Mono&display=swap" rel="stylesheet">
+  <link rel="icon" href="/images/favicon.png">
+
+  <link rel="stylesheet" href="/css/animate.css">
+  <link rel="stylesheet" href="/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="/css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="/fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="/fonts/fontawesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="/css/aos.css">
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="/css/style.css">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158958448-1"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+	
+  gtag('config', 'UA-158958448-1');
+  </script>
+</head>
+
+<body>
+  <?php include "./components/navbar.html" ?>
+  <!-- END header -->
+
+  <div class="slider-item innerp overlay" data-stellar-background-ratio="0.5"
+    style="background-image: url('/images/color/069_069_069.jpg'); height:80px; min-height: 80px">
+    
+  </div>
+
+  
+  <div class="section portfolio-section">
+    <div class="container">
+      <div class="row mb-5 justify-content-center">
+        <img data-aos="fade-right" data-aos-delay="" src="/images/brand_space/IE_B_Text.png" style="width:30vh;">
+        <div class="col-md-25 text-center">
+		  <h1 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="" style="font-size:8vh">Contact Form Submission</h1>
+		  <h3 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="" style="font-size:3vh">
+        <?php echo $mailmessage; ?>
+      </h3>
+      <h4 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="" style="font-size:3vh"> You will be returned the contact page in 5 seconds </h4>
+
+      <div data-aos="fade-right" data-aos-delay="">
+        <p style="text-align: center;"><a href="/index.html" class="btn btn-outline-black">Click Here To Return Home</a></p>
+      </div>
+
+      
+          
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include "./components/footer.html" ?>
+  <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#ffc107" /></svg></div> 
+
+        
+  <?php include "./components/commonScripts.html" ?>
+
+</body>
+</html>
