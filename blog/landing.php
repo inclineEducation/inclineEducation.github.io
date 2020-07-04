571797445c62ccce613f0e153b538c58ee1e75d9
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-07-03 22:23:12 -0700

    testing php

--- blog/landing.php
+++ blog/landing.php
@@ -45,9 +45,9 @@ $password = "Fl@pdc@4@%rJ";
 
 
 $conn = new mysqli($servername, $username, $password);
-$blogTable = $conn->query("SELECT * FROM inclineeducation.blog");
+$blogTable = $conn->query("SELECT * FROM inclineeducation.blog ORDER BY date DESC");
 $numPosts = $conn->query("SELECT COUNT(*) FROM inclineeducation.blog");
-echo gettype($numPosts);
+echo $numPosts->fetch_all()[0];
 $conn->close();
 
 
