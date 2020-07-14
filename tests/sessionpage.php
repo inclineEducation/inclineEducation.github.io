2e4b4eb4ae0f39fc602fd818c9ffedf62c57d298
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-07-13 21:20:48 -0700

    testing authentication

--- tests/sessionpage.php
+++ tests/sessionpage.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
-    foreach ($_SESSION["payload"] as $key => $value){
-        echo "<p>".$key.": ".$value."</p>";
+    if (array_key_exists("payload", $_SESSION)){
+        foreach ($_SESSION["payload"] as $key => $value){
+            echo "<p>".$key.": ".$value."</p>";
+        }
+    } else {
+        echo "<p>NOT SIGNED IN</p>";
     }
 ?>
