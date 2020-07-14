c49988af65887f64883866ddcc2b2eaa70e67353
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-07-13 21:02:02 -0700

    testing authentication

--- tests/logout.php
+++ tests/logout.php
@@ -1,10 +1,13 @@
 <?php
     $token = htmlspecialchars($_POST['token']);
+    $sestoken = $_SESSION['token'];
     if ($_SESSION['token'] == $token){
         session_unset();
         session_destroy();
         echo "logged out";
     } else {
         echo "token doesnt match";
+        echo "expected: $token";
+        echo "got: $sestoken";
     }
 ?>
