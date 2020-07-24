3e7bf48d7ace5b3b66f0c8eb0715a385be25cdf7
qwertng <qwertngchris@gmail.com>
2020-07-23 20:21:03 -0600

    join page

---
+++ join.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Incline Education - Mentorship</title>
+  <meta name="title" content="Incline Education Mentorship Program">
+  <meta name="description" content="Incline Education’s Mentorship Program is a platform for high-school 
+  students to be matched with post-secondary students to guide them in their academic and career 
+  endeavours. Sign up to be either a mentor or a mentee, and we’ll pair you up with someone who matches 
+  your interests and aspirations.">
+  <meta name="navpage" content="services" />
+  <!--Social Media Preview Stuff-->
+  <meta property="og:site_name" content="Incline Education" />
+  <meta property="og:title" content="Incline Education Mentorship Program" />
+  <meta property="og:description" content="Incline Education’s Mentorship Program is a platform for high-school students to be matched with post-secondary students to guide them in their academic and career endeavours. Sign up to be either a mentor or a mentee, and we’ll pair you up with someone who matches your interests and aspirations." />
+  <meta property="og:image" content="https://inclineedu.org/images/brand_space/IEMentorship.png" />
+  <meta property="og:url" content="https://inclineedu.org/mentorship" />
+  <meta property="og:type" content="website" />
+  <meta property="fb:app_id" content="645462402679378" />
+  <meta property="fb:admins" content="100000242873868"/>
+  <meta name="keywords" content="incline,education,mentorship,panel,discussions,secondary,education,school,
+  post,high,students,teachers,parents,community,advancing,charity,blog,university,panel">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  
+
+
+  <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display:400,400i|Roboto+Mono&display=swap" rel="stylesheet">
+  <link rel="icon" href="images/favicon.png">
+
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="css/style.css">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158958448-1"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+	
+  gtag('config', 'UA-158958448-1');
+  </script>
+
+  <!-- React Scripts -->
+  <!--
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+  -->
+
+  <!-- JQuery Source -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+  <?php include "components/navbar.html" ?>
+  <!-- END header -->
+
+  <div class="slider-item innerp overlay" data-stellar-background-ratio="0.5"
+    style="background-image: url('images/group.jpg');">
+    <div class="container">
+      <div class="row slider-text align-items-center justify-content-center text-center">
+        <div class="col-lg-12 col-sm-12">
+          <h1 class="mb-4" data-aos="fade-up" data-aos-delay="">Join Us</h1>
+		  <p class="custom-breadcrumbs" data-aos="fade-up" data-aos-delay="100"><a href="/index.php">Home</a> <span class="mx-3">/</span> Mentorship</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="section portfolio-section" style="padding-bottom: 4em;">
+    <div class="container">
+      <div class="row mb-5 justify-content-center">
+        <div class="col-md-25 text-center" style="padding: 1em;">
+          <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="">Why Join Incline Education?</h2>
+          <p data-aos="fade-right" data-aos-delay="100">
+          Personal development is ingrained in our culture. Whether you want to learn how to run your own startup or develop your leadership skills, 
+          you can rest assured that our team and advisory board will be there to support you. 
+          </p>
+          <br>
+
+          <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="">Who Are We Looking For?</h2>
+          <p data-aos="fade-right" data-aos-delay="100">
+          We are looking for motivated individuals who are passionate about education. At Incline Education, no two team members are alike. Our team consists 
+          of individuals from 8+ countries around the world and spanning 10+ different post-secondary programs. 
+          </p>
+          <br>
+
+          <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="">Our Team Members Have Received Offers and Worked For These Organizations:</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+<!--Services-->
+
+<?php include "./components/services.html" ?>
+
+<?php include "./components/testimonials.html" ?>
+
+   <div class="bg-primary py-5">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-lg-7">
+          <h3 class="text-white mb-2 font-weight-normal">Let's do more together</h3>
+
+        
+          <p class="mb-0"><a href="contact" class="btn btn-outline-white px-4 py-3">Get In Touch!</a></p>
+        </div>
+      </div>
+
+    </div>
+  </div> 
+
+  <?php include "components/footer.html" ?>
+  <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#ffc107" /></svg></div> 
+
+  <?php include "./components/commonScripts.html" ?>
+
+</body>
+</html>
---
+++ press.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Incline Education - Mentorship</title>
+  <meta name="title" content="Incline Education Mentorship Program">
+  <meta name="description" content="Incline Education’s Mentorship Program is a platform for high-school 
+  students to be matched with post-secondary students to guide them in their academic and career 
+  endeavours. Sign up to be either a mentor or a mentee, and we’ll pair you up with someone who matches 
+  your interests and aspirations.">
+  <meta name="navpage" content="services" />
+  <!--Social Media Preview Stuff-->
+  <meta property="og:site_name" content="Incline Education" />
+  <meta property="og:title" content="Incline Education Mentorship Program" />
+  <meta property="og:description" content="Incline Education’s Mentorship Program is a platform for high-school students to be matched with post-secondary students to guide them in their academic and career endeavours. Sign up to be either a mentor or a mentee, and we’ll pair you up with someone who matches your interests and aspirations." />
+  <meta property="og:image" content="https://inclineedu.org/images/brand_space/IEMentorship.png" />
+  <meta property="og:url" content="https://inclineedu.org/mentorship" />
+  <meta property="og:type" content="website" />
+  <meta property="fb:app_id" content="645462402679378" />
+  <meta property="fb:admins" content="100000242873868"/>
+  <meta name="keywords" content="incline,education,mentorship,panel,discussions,secondary,education,school,
+  post,high,students,teachers,parents,community,advancing,charity,blog,university,panel">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  
+
+
+  <link href="https://fonts.googleapis.com/css?family=DM+Serif+Display:400,400i|Roboto+Mono&display=swap" rel="stylesheet">
+  <link rel="icon" href="images/favicon.png">
+
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="css/style.css">
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158958448-1"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+	
+  gtag('config', 'UA-158958448-1');
+  </script>
+
+  <!-- React Scripts -->
+  <!--
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+  -->
+
+  <!-- JQuery Source -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+  <?php include "components/navbar.html" ?>
+  <!-- END header -->
+
+  <div class="slider-item innerp overlay" data-stellar-background-ratio="0.5"
+    style="background-image: url('images/group.jpg');">
+    <div class="container">
+      <div class="row slider-text align-items-center justify-content-center text-center">
+        <div class="col-lg-12 col-sm-12">
+          <h1 class="mb-4" data-aos="fade-up" data-aos-delay="">Mentorship</h1>
+		  <p class="custom-breadcrumbs" data-aos="fade-up" data-aos-delay="100"><a href="/index.php">Home</a> <span class="mx-3">/</span> Mentorship</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="section portfolio-section" style="padding-bottom: 4em;">
+    <div class="container">
+      <div class="row mb-5 justify-content-center">
+        <div class="col-md-25 text-center" style="padding: 1em;">
+          <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="">What is the Incline Education Mentorship Program?</h2>
+          <p data-aos="fade-right" data-aos-delay="100">
+            The Incline Education Mentorship Program is a platform for high-school students to be matched with a post-secondary student to 
+            guide them in their academic and career endeavours. You can sign up to be either a mentor or a mentee, and we will pair you 
+            up with someone who matches your interests and aspirations. 
+          </p>
+          <br>
+
+          <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="">How Does It Work?</h2>
+          <ol style="list-style-position: inside; line-height: 1em;">
+            <li data-aos="fade-right" data-aos-delay="100"> Fill out a 2-minute survey about your interests
+              and aspirations to help us match you with the best mentor.
+            </li>
+            <br>
+            <li data-aos="fade-right" data-aos-delay="100">
+              Within 1 week, you will receive an email from Incline Education connecting you with a mentor.
+            </li>
+            <br>
+            <li data-aos="fade-right" data-aos-delay="100">
+              Set up a time to meet your mentor and have a digital coffee chat.
+            </li>
+
+          </ol>
+          <!--
+          <p data-aos="fade-right" data-aos-delay="100">
+            It just takes you two minutes to sign up to be a mentor or mentee!
+          </p>
+          -->
+          <br>
+          <div class = "row" data-aos="fade-right" data-aos-delay="100">
+            <div class="col-lg-6 order-1">
+              <p style="text-align: center;"><a href="/mentee" class="btn btn-outline-black">Sign up to meet a mentor!</a></p>
+            </div>
+            <div class="col-lg-6 order-1">
+              <p style="text-align: center;"><a href="/mentor" class="btn btn-outline-black">Sign up to mentor a student!</a></p>
+            </div>
+          </div>
+          <!--
+          <p data-aos="fade-right" data-aos-delay="100">
+            
+            We understand that students want to be exceptional when they go into university which means that they would not want to hear about the experiences of mediocre students. That is why we have brought together a team consisting of high achieving students from various backgrounds and fields of study. We are committed to bringing the best to every event that we hold to ensure that students are able to get the most out of this experience.
+            
+          </p>
+          -->
+          <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="" style="padding-top:2em;">
+            Frequently Asked Questions
+          </h2>
+          <ul style="list-style-type: none; line-height: 1em;">
+            <li data-aos="fade-right" data-aos-delay="100">
+              <p><strong>Do I have to pay for this program?</strong></p>
+              <p>No! The Incline Education Mentorship Program is offered completely free of charge.</p>
+            </li>
+            <br><br>
+            <li data-aos="fade-right" data-aos-delay="100">
+              <p><strong>I haven't heard from Incline Education in a week - what should I do?</strong></p>
+              <p>Send us an email at <a href="mailto:contact@inclineedu.org"target="_blank">contact@inclineedu.org</a> to check-in regarding your application!</p>
+            </li>
+            <br><br>
+            <li data-aos="fade-right" data-aos-delay="100">
+              <p><strong>Who can be a mentee?</strong></p>
+              <p>The program is open to anyone who is considering post-secondary studies!</p>
+            </li>
+            <br><br><br>
+            <li data-aos="fade-right" data-aos-delay="100">
+              <p>If you have any other questions or concerns, please don't hesitate to contact us at <a href="mailto:contact@inclineedu.org"target="_blank">contact@inclineedu.org</a>.</p>
+            </li>
+          </ul>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+<!--Services-->
+
+<?php include "./components/services.html" ?>
+
+<?php include "./components/testimonials.html" ?>
+
+   <div class="bg-primary py-5">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-lg-7">
+          <h3 class="text-white mb-2 font-weight-normal">Let's do more together</h3>
+
+        
+          <p class="mb-0"><a href="contact" class="btn btn-outline-white px-4 py-3">Get In Touch!</a></p>
+        </div>
+      </div>
+
+    </div>
+  </div> 
+
+  <?php include "components/footer.html" ?>
+  <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#ffc107" /></svg></div> 
+
+  <?php include "./components/commonScripts.html" ?>
+
+</body>
+</html>
