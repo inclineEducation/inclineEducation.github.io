b213f4652539898c151f7528b380e7d794dc067a
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-08-01 18:04:48 -0700

    fix pull api

--- admin/api/pull.php
+++ admin/api/pull.php
@@ -1,6 +1,6 @@
 <?php
 if ( $_POST['payload'] ) {
-shell_exec( 'cd /home/bitnami/htdocs && git reset –hard HEAD && git pull' );
+shell_exec( 'cd /home/bitnami/htdocs && git reset –-hard HEAD && git pull' );
 }
-
+//q4RspMySWY4U7iunTHnR
 ?>pull completed
