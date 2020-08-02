dc296686ef757050ccaefebf9f55cf450eae5b44
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-08-01 18:38:46 -0700

    changing pull api

--- admin/api/pull.php
+++ admin/api/pull.php
@@ -7,7 +7,7 @@ if ( $_POST['payload'] ) {
 //$result = shell_exec( 'cd /home/bitnami/htdocs && sudo git reset –-hard HEAD && sudo git pull' );
 echo "pulling\n";
 echo passthru("echo 'passthru test'");
-echo passthru( 'cd /home/bitnami/htdocs && sudo git reset --hard HEAD && sudo git pull 2>&1' );
+echo passthru( 'cd /home/bitnami/htdocs 2>&1 && sudo git reset --hard 2>&1 HEAD && sudo git pull 2>&1' );
 }
 //q4RspMySWY4U7iunTHnR
 ?>pull completed
