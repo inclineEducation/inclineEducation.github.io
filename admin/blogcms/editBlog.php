2b46a221465ffd3e5197eefeef69a3c7ebcf0ba2
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-08-19 21:48:29 -0700

    blog preview

--- admin/blogcms/editBlog.php
+++ admin/blogcms/editBlog.php
@@ -18,6 +18,8 @@
             $uri = $row['URI'];
             $live = $row['live'];
             $new = false;
+            //This needs to be changed once titleimage field is added.
+            $titleImage = $row['titleImage'] ? $row['titleImage'] : "/blog/i/$id/title.jpg";
         }else{
             echo "error";
             $title = "";
@@ -34,7 +36,6 @@
         $uri=filter_var($_GET['URI'], FILTER_SANITIZE_URL);
         $post=$conn->query("SELECT * FROM inclineeducation.blog WHERE URI = '".$uri."'");
         if (mysqli_num_rows($post) == 1) {
-            echo "post found";
             $row = $post->fetch_assoc();
             $title = $row['title'];
             $subtitle = $row['subtitle'];
@@ -45,7 +46,9 @@
             $id = $row['id'];
             $live = $row['live'];
             $new = false;
-        }else{
+            //This needs to be changed once titleimage field is added.
+            $titleImage = $row['titleImage'] ? $row['titleImage'] : "/blog/i/$id/title.jpg";
+        }else{  /* edit without valid URI or ID -> redirect to new */
             echo "error";
             $title = "";
             $subtitle = "";
@@ -56,9 +59,10 @@
             $uri = "";
             $id = "";
             $new = true;
+            //This needs to be changed once titleimage field is added.
         }
     }
-    else{
+    else{   /* New blog article */
         $title = "";
         $subtitle = "";
         $firstName = "";
@@ -195,11 +199,16 @@ input:focus{
 
 </style>
 </head>
-<body style="background-color: #2a2a2a">
+<body>
 <!--APPEND .PHP TO SUBMITHTML WHEN IN DEVELOPMENT ENVIRONMENT-->
+<div style="background-color: #2a2a2a">
+<p>
+    <?php echo "id: $id";?>
+</p>
 <form method="post" id="postdata" action="submitHTML">
     <?php
     $uriField = $new ? '<input id="uri" name="URI" type="text" placeholder="URI" style="font-size: 1.5rem">' : '';
+    //set "Is Live" radio button things
     $liveField = isset($live) ? (
                     $live ? <<< TEXT
                     <input type="radio" id="live" name="status" value="1" required checked>
@@ -226,13 +235,13 @@ input:focus{
         </tr>
 
         <tr>
-        <th><input name="subtitle" type="subtitle" placeholder="Subtitle" value="$subtitle" style="width:90vw; font-size:2rem;"></th>
+        <th><input id="subtitle" name="subtitle" type="subtitle" placeholder="Subtitle" value="$subtitle" style="width:90vw; font-size:2rem;"></th>
         </tr>
 
         <tr>
-        <th><input name="firstName" type="text" placeholder="First Name" value="$firstName" style="width:30vw; font-size: 1.5rem">
-        <input name="lastName" type="text" placeholder="Last Name" value="$lastName" style="width:30vw; font-size: 1.5rem">
-        <input name="date" type="date" placeholder="date" value="$date" style="width:29vw; font-size: 1.5rem"></th>
+        <th><input id="firstname" name="firstName" type="text" placeholder="First Name" value="$firstName" style="width:30vw; font-size: 1.5rem">
+        <input id="lastname" name="lastName" type="text" placeholder="Last Name" value="$lastName" style="width:30vw; font-size: 1.5rem">
+        <input id="date" name="date" type="date" placeholder="date" value="$date" style="width:29vw; font-size: 1.5rem"></th>
         </tr>
 
         <tr>
@@ -257,7 +266,52 @@ input:focus{
 FORM;
 ?>
 </form>
+</div>
+
+<div id="titleImage" class="slider-item innerp overlay" data-stellar-background-ratio="0.5"
+style="background-image: url('/images/color/gradient.jpg'); height: calc(60vh); min-height: 400px;">
+<div class="container">
+    <div class="row slider-text align-items-center justify-content-left text-left titleText" style="height: calc(60vh); min-height: 400px;">
+    <div class="col-lg-12 col-sm-12">
+        <h1 id="titleP" data-aos="fade-up" style="font-size:calc(1.75rem + 0.75vw);"></h1>
+        <h2 id="subP" data-aos="fade-up" style="color: white; font-size: calc(1rem + 1vw);"></h2>
+        <p data-aos="fade-up" style="font-size: calc(0.6rem + 0.25vw);"><em id="metaP"></em></p>
+    </div>
+    </div>
+</div>
+</div>
+
+<div class="section portfolio-section" style="padding-bottom: 4em; padding-top: 2em;">
+<div class="container justify-content-center" style="width: calc(30rem + 30vw);max-width:calc(90vw);">
+    <div class="row mb-5 justify-content-center">
+    <div id="contentP" class="col-md-25" style="padding: 1em;" data-aos="fade-left" data-aos-offset="-200">
+    </div>
+    </div>
+</div>
+</div>
+
+  
+<!--Services-->
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/components/services.html" ?>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/components/testimonials.html" ?>
+
+   <div class="bg-primary py-5">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-lg-7">
+          <h3 class="text-white mb-2 font-weight-normal">Let's do more together</h3>
+
+        
+          <p class="mb-0"><a href="/contact" class="btn btn-outline-white px-4 py-3">Get In Touch!</a></p>
+        </div>
+      </div>
+
+    </div>
+  </div> 
 <script>
+    //Code Area
     var cm = CodeMirror.fromTextArea(document.getElementById("content"),
         {
             mode : "htmlmixed",
@@ -268,10 +322,68 @@ FORM;
             theme: "pastel-on-dark"
         }
     );
+
+    //update author and date preview function
+    function updateMeta(){
+        var date = new Date($('#date').val());
+        $('#metaP').html(
+            'Posted by ' + 
+            $('#firstname').val() + ' ' +
+            $('#lastname').val() + ' on ' + 
+            date.toDateString()
+        );
+    }
+
+    //update preview on load
+    $(window).on('load', function(){
+        console.log("loaded");
+        $('#titleP').html($('#title').val());
+        $('#subP').html($('#subtitle').val());
+        $('#contentP').html(cm.getValue());
+        updateMeta();
+        $('#titleImage').css("background-image", "url('<?php echo $titleImage?>')")
+        
+    })
+
     $("#title").change(function(){
-        console.log("change");
-            $('#uri').val($(this).val().replace(/ /g,"_").replace(/[^a-zA-Z0-9\_\-]/g, ''));
+        //update URI
+        console.log("title change");
+        $('#uri').val($(this).val().replace(/ /g,"_").replace(/[^a-zA-Z0-9\_\-]/g, ''));
+
+        //update title preview
+        $('#titleP').html($(this).val());
         });
+    
+    //update live preview
+    cm.on("change",function(cm){
+        console.log("content change");
+        $('#contentP').html(cm.getValue());
+    });
+
+    //update subtitle preview
+    $("#subtitle").change(function(){
+        $('#subP').html($(this).val());
+    });
+
+    //update author and date preview
+    $('#firstname').change(updateMeta());
+    $('#lastname').change(updateMeta());
+    $('#date').change(updateMeta());
+
+
+
+
 </script>
+<script src="/js/element-updates.js"></script>
+  <script src="/js/jquery-3.2.1.min.js"></script>
+  <script src="/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="/js/popper.min.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
+  <script src="/js/owl.carousel.min.js"></script>
+  <script src="/js/jquery.waypoints.min.js"></script>
+  <script src="/js/jquery.fancybox.min.js"></script>
+  <script src="/js/jquery.stellar.min.js"></script>
+  <script src="/js/aos.js"></script>
+  <script src="/js/main.js"></script>
 </body>
 </html>
