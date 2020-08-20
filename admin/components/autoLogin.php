e65423ff57c62046982e7f6bf5838480dbcb9fff
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-08-19 22:04:15 -0700

    headers to black in blog posts

--- admin/components/autoLogin.php
+++ admin/components/autoLogin.php
@@ -1,4 +1,5 @@
 <?php
+    //This component automatically sets the user session, or redirects user to login page if no user session is found.
     if ((!array_key_exists("user", $_SESSION)) && $_SERVER['REMOTE_ADDR'] != '127.0.0.1'){
         header("Location: /admin/login?redirect=".$_SERVER['REQUEST_URI']);
     }
