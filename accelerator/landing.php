17b0d13434c5f2d6230805b0be19a4ae43dd5f28
Yoruio <zeleidu@gmail.com>
2020-12-26 20:17:42 -0700

    quick links header

--- accelerator/landing.php
+++ accelerator/landing.php
@@ -75,10 +75,10 @@
         <div class="col-md-10 text-center">
         <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="" id="table_of_contents">Quick Links</h2>
           <ul style="list-style-type: none">
-            <li data-aos="fade-right"><a href="#purpose">Purpose Of This Program<a></li>
-            <li data-aos="fade-right"><a href="#provide">What We Provide<a></li>
-            <li data-aos="fade-right"><a href="#goal">Our Goal For You<a></li>
-            <li data-aos="fade-right"><a href="#application">Application<a></li>
+            <p><a href="#purpose">Purpose Of This Program<a></p>
+            <p><a href="#provide">What We Provide<a></p>
+            <p><a href="#goal">Our Goal For You<a></p>
+            <p><a href="#application">Application<a></p>
           </ul>
           <h2 class="mb-4 section-title" data-aos="fade-right" data-aos-delay="" id="purpose">Purpose Of This Program</h2>
           <p data-aos="fade-right">
